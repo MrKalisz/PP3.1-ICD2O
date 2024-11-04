0517df9871aca88de00e30d5f810081a2dda06c4
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2024-11-03 23:04:52 -0500

    Update and rename PP2_1_test.py to PP3_1_test.py

---
+++ PP3_1_test.py
@@ -0,0 +1,64 @@
+import os.path
+import sys
+import PP3_1
+
+def test_q1_1(capsys):
+
+	try:
+		exists = os.path.exists("PP3_1.py")
+		assert exists
+	except:
+		sys.exit()
+
+	PP3_1.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: In:\n"
+
+def test_q2_1(capsys):
+
+	try:
+		exists = os.path.exists("PP3_1.py")
+		assert exists
+	except:
+		sys.exit()
+
+	PP3_1.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 1 2 3 4 5\n"
+
+def test_q1_2(capsys):
+
+	try:
+		exists = os.path.exists("PP3_1.py")
+		assert exists
+	except:
+		sys.exit()
+
+	PP3_1.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 0 1 2 3 4 5 6 7 8 9\n"
+
+def test_q2_2(capsys):
+
+	try:
+		exists = os.path.exists("PP3_1.py")
+		assert exists
+	except:
+		sys.exit()
+
+	PP3_1.q2()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 1 2 3 4 5 6 7 8 9 10\n"
+
+def test_q1_3(capsys):
+
+	try:
+		exists = os.path.exists("PP3_1.py")
+		assert exists
+	except:
+		sys.exit()
+
+	PP3_1.q1()
+	captured = capsys.readouterr()
+	assert captured.out == "In: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19\n"
+
